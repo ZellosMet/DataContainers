8d3cb9801534d0aa0cbc6832d034c7713eb039ac
Zello <Zello@DESKTOP-IBMII35>
2023-08-25 23:34:50 +0900

    Separated in progress

--- List2/List2.cpp
+++ List2/List2.cpp
@@ -171,164 +171,39 @@ public:
 	}
 };
 
-	constIterator cbegin()const
-	{
-		return Head;
-	}
-	constIterator cend()const
-	{
-		return nullptr;
-	}
-	constReversIterator rcbegin()const
-	{
-		return Tail;
-	}
-	constReversIterator rcend()const
-	{
-		return nullptr;
-	}
-	Iterator begin()
-	{
-		return Head;
-	}
-	Iterator end()
-	{
-		return nullptr;
-	}
-	ReversIterator rbegin()
-	{
-		return Tail;
-	}
-	ReversIterator rend()
-	{
-		return nullptr;
-	}
+constIterator cbegin()const;	
+constIterator cend()const;
+constReversIterator rcbegin()const;
+constReversIterator rcend()const;
+Iterator begin();
+Iterator end();
+ReversIterator rbegin();
+ReversIterator rend();
 
-	List()
-	{
-		Head = Tail = nullptr;
-#ifdef DEBUG
-		std::cout << "LConstructor:\t" << this << std::endl;
-#endif // DEBUG
-
-	}
-	List(const std::initializer_list<T>& il):List()
-	{
-		for (T i : il) push_back(i);
-	}
-	List(const List<T>& other):List()
-	{
-		*this = other;
-	}
-	~List()
-	{
-		//while (Head)pop_front();
-		while (!Tail)pop_back();
-#ifdef DEBUG
-		std::cout << "LDestructor:\t" << this << std::endl;
-#endif // DEBUG
-
-	}
+List();
+List(const std::initializer_list<T>& il);
+List(const List<T>& other);
+~List();
 
 						//Operators
-	List<T>&operator=(const List<T>& other)
-	{
-		if (this == &other) return *this;
-		while (Head)pop_front();
-		for (Element* Temp = other.Head; Temp; Temp = Temp->pNext)push_back(Temp->Data);
-#ifdef DEBUG
-		std::cout << "LCopyAssignment:\t" << std::endl;
-#endif // DEBUG
+List<T>& operator=(const List<T>& other);
 
-		return *this;
-	}
-					//Ading elements
-	void push_front(T Data)
-	{
-		if(!Head && !Tail) Head = Tail = new Element(Data);
-		else Head = Head->pPrev = new Element(Data, Head);
-		size++;
-	}
-	void push_back(T Data)
-	{
-		if (!Head && !Tail) Head = Tail = new Element(Data);
-		else Tail = Tail->pNext = new Element(Data, nullptr, Tail);
-		size++;
-	}
-	void insert(T Data, int index)
-	{
-		if (!index) return push_front(Data);
-		if (index > size) return;
-		Element* Temp;
-		if (index < size/2)
-		{
-			Temp = Head;
-			for (int i = 0; i < index; i++)Temp = Temp->pNext;
-		}
-		else
-		{
-			Temp = Tail;
-			for (int i = 0; i < size-index-1; i++)Temp = Temp->pPrev;
-		}
-		Temp->pPrev = Temp->pPrev->pNext = new Element(Data, Temp, Temp->pPrev);
-		size++;
-	}
+					//Adding elements
+void push_front(T Data);
+void push_back(T Data);
+void insert(T Data, int index);
 
 					//Removing elements
-	void pop_front()
-	{
-		if (!Head && !Tail) return;
-		if (Head == Tail)
-		{
-			delete Head;
-			Head = Tail = nullptr;
-			return;
-		}
-		Head = Head->pNext;
-		delete Head->pPrev;		
-		Head->pPrev = nullptr;
-		size--;
-	}
-	void pop_back()
-	{
-		if (Head == Tail)return pop_front();
-		Tail = Tail->pPrev;
-		delete Tail->pNext;
-		Tail->pNext = nullptr;
-		size--;
-	}
-	void erase(int index)
-	{
-		if (!index) return pop_front();
-		if (index >= size) return;
-		Element* Temp;
-		if (index < size / 2)
-		{
-			Temp = Head;
-			for (int i = 0; i < index; i++)Temp = Temp->pNext;
-		}
-		else
-		{
-			Temp = Tail;
-			for (int i = 0; i < size - index - 1; i++)Temp = Temp->pPrev;
-		}
-		Temp->pNext->pPrev = Temp->pPrev;
-		Temp->pPrev->pNext = Temp->pNext;
-		delete Temp;
-		size--;
-	}
-				//Methods
-	void print()const
-	{
-		for (Element* Temp = Head; Temp; Temp = Temp->pNext)
-			std::cout << Temp->pPrev << "\t" << Temp->Data << "\t" << Temp->pNext << std::endl;
-	}
-	void revprint()const
-	{
-		for (Element* Temp = Tail; Temp; Temp = Temp->pPrev)
-			std::cout << Temp->pPrev << "\t" << Temp->Data << "\t" << Temp->pNext << std::endl;
-	}
-	template<typename T>friend List<T> operator+(const List<T>& left, const List<T>& right);
+void pop_front();
+void pop_back();
+void erase(int index);
+
+						//Methods
+void print()const;
+void revprint()const;
+
+						//Friends
+template<typename T>friend List<T> operator+(const List<T>& left, const List<T>& right);
 };
 
 template<typename T>List<T> operator+(const List<T>& left, const List<T>& right)
@@ -337,6 +212,163 @@ template<typename T>List<T> operator+(const List<T>& left, const List<T>& right)
 	for (typename List<T>::constIterator it = right.cbegin(); it != right.cend(); ++it)
 		cat.push_back(*it);
 	return cat;
+}
+
+template<typename T>typename List<T>::constIterator List<T>::cbegin()const
+{
+	return Head;
+}
+template<typename T>typename List<T>::constIterator List<T>::cend()const
+{
+	return nullptr;
+}
+template<typename T>typename List<T>::constReversIterator List<T>::rcbegin()const
+{
+	return Tail;
+}
+template<typename T>typename List<T>::constReversIterator List<T>::rcend()const
+{
+	return nullptr;
+}
+template<typename T>typename List<T>::Iterator List<T>::begin()
+{
+	return Head;
+}
+template<typename T>typename List<T>::Iterator List<T>::end()
+{
+	return nullptr;
+}
+template<typename T>typename List<T>::ReversIterator List<T>::rbegin()
+{
+	return Tail;
+}
+template<typename T>typename List<T>::ReversIterator List<T>::rend()
+{
+	return nullptr;
+}
+
+//Adding elements
+template<typename T> void List<T>::push_front(T Data)
+{
+	if (!Head && !Tail) Head = Tail = new Element(Data);
+	else Head = Head->pPrev = new Element(Data, Head);
+	size++;
+}
+template<typename T> void List<T>::push_back(T Data)
+{
+	if (!Head && !Tail) Head = Tail = new Element(Data);
+	else Tail = Tail->pNext = new Element(Data, nullptr, Tail);
+	size++;
+}
+template<typename T> void List<T>::insert(T Data, int index)
+{
+	if (!index) return push_front(Data);
+	if (index > size) return;
+	Element* Temp;
+	if (index < size / 2)
+	{
+		Temp = Head;
+		for (int i = 0; i < index; i++)Temp = Temp->pNext;
+	}
+	else
+	{
+		Temp = Tail;
+		for (int i = 0; i < size - index - 1; i++)Temp = Temp->pPrev;
+	}
+	Temp->pPrev = Temp->pPrev->pNext = new Element(Data, Temp, Temp->pPrev);
+	size++;
+}
+
+//Removing elements
+template<typename T> void List<T>::pop_front()
+{
+	if (!Head && !Tail) return;
+	if (Head == Tail)
+	{
+		delete Head;
+		Head = Tail = nullptr;
+		return;
+	}
+	Head = Head->pNext;
+	delete Head->pPrev;
+	Head->pPrev = nullptr;
+	size--;
+}
+template<typename T> void List<T>::pop_back()
+{
+	if (Head == Tail)return pop_front();
+	Tail = Tail->pPrev;
+	delete Tail->pNext;
+	Tail->pNext = nullptr;
+	size--;
+}
+template<typename T> void List<T>::erase(int index)
+{
+	if (!index) return pop_front();
+	if (index >= size) return;
+	Element* Temp;
+	if (index < size / 2)
+	{
+		Temp = Head;
+		for (int i = 0; i < index; i++)Temp = Temp->pNext;
+	}
+	else
+	{
+		Temp = Tail;
+		for (int i = 0; i < size - index - 1; i++)Temp = Temp->pPrev;
+	}
+	Temp->pNext->pPrev = Temp->pPrev;
+	Temp->pPrev->pNext = Temp->pNext;
+	delete Temp;
+	size--;
+}
+//Methods
+template<typename T> void List<T>::print()const
+{
+	for (Element* Temp = Head; Temp; Temp = Temp->pNext)
+		std::cout << Temp->pPrev << "\t" << Temp->Data << "\t" << Temp->pNext << std::endl;
+}
+template<typename T> void List<T>::revprint()const
+{
+	for (Element* Temp = Tail; Temp; Temp = Temp->pPrev)
+		std::cout << Temp->pPrev << "\t" << Temp->Data << "\t" << Temp->pNext << std::endl;
+}
+template<typename T> List<T>::List()
+{
+	Head = Tail = nullptr;
+#ifdef DEBUG
+	std::cout << "LConstructor:\t" << this << std::endl;
+#endif // DEBUG
+
+}
+template<typename T> List<T>::List(const std::initializer_list<T>& il) :List()
+{
+	for (T i : il) push_back(i);
+}
+template<typename T> List<T>::List(const List<T>& other) :List()
+{
+	*this = other;
+}
+template<typename T> List<T>::~List()
+{
+	while (!Tail)pop_back();
+#ifdef DEBUG
+	std::cout << "LDestructor:\t" << this << std::endl;
+#endif // DEBUG
+
+}
+
+//Operators
+template<typename T>List<T>& List<T>::operator=(const List<T>& other)
+{
+	if (this == &other) return *this;
+	while (Head)pop_front();
+	for (Element* Temp = other.Head; Temp; Temp = Temp->pNext)push_back(Temp->Data);
+#ifdef DEBUG
+	std::cout << "LCopyAssignment:\t" << std::endl;
+#endif // DEBUG
+
+	return *this;
 }
 
 //#define BASE_CHECK
