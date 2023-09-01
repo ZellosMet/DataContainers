50dd5a7898ff9fd26ae0f87b3024e18033e6c596
Zello <Zello@DESKTOP-IBMII35>
2023-09-01 11:12:09 +0900

    Recursion DONE

--- BinaryTree/BinaryTree.cpp
+++ BinaryTree/BinaryTree.cpp
@@ -1,7 +1,44 @@
 #include<iostream>
 
+class Tree
+{
+	class Element
+	{
+		int  Data;
+		Element* pLeft;
+		Element* pRight;
+	public:
+		Element(int Data, Element* pLeft = nullptr, Element* pRaght = nullptr) : Data(Data), pLeft(pLeft), pRight(pRight)
+		{
+			std::cout << "EConstructor:\t" << this << std::endl;
+		}
+		~Element()
+		{
+			std::cout << "EDestructor:\t" << this <<std::endl;
+		}
+	}*Root;
+
+public:
+	Tree() :Root(nullptr)
+	{
+		std::cout << "EConstructor:\t" << this << std::endl;
+	}
+	Tree(Element* Root = nullptr) : Root(Root)
+	{
+		std::cout << "EConstructor:\t" << this << std::endl;
+	}
+	~Tree()
+	{
+		std::cout << "TDestructor:\t" << this << std::endl;
+	}
+	void insert(int Data)
+	{
+		
+	}
+
+};
+
 void main()
 {
 	setlocale(LC_ALL, "");
-
 }
---
+++ Recursion/Recursion.cpp
@@ -0,0 +1,76 @@
+#include<iostream>
+
+void Elevator(int floor);
+int Factorial(int n);
+double Power(double a, int n);
+double Fibonacci(int n);
+
+//#define ELEVATOR
+//#define FACTORIAL
+//#define POWER
+#define FIBONACCI
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	int n, a;
+
+#ifdef ELEVATOR
+	std::cout << "Введите номер этожа: "; std::cin >> n;
+	elevator(n);
+#endif 
+
+#ifdef FACTORIAL
+	std::cout << "Введите число: "; std::cin >> n;
+	std::cout << n << "! = " << Factorial(n);
+#endif 
+
+#ifdef  POWER
+	std::cout << "Введите число и степень: "; std::cin >> a >> n;
+	std::cout << "Число " << a << " в степени " << n << " = " << Power(a, n);
+#endif
+
+#ifdef FIBONACCI
+	std::cout << "Введите сколько чисел ряда Фибоначчи нужно вывести: "; std::cin >> n;
+	for (int i = 1; i <= n; i++)
+		std::cout << Fibonacci(i) << "\t";
+
+	std::cout << std::endl;
+
+	std::cout << "Введите предел ряда Фибоначчи: "; std::cin >> n;
+	for (int i = 1; n >= Fibonacci(i); i++)
+		std::cout << Fibonacci(i) << "\t";
+#endif 
+
+}
+
+void Elevator(int floor)
+{
+	if (floor == 0)
+	{
+		std::cout << "Вы в подвале" << std::endl;
+		return;
+	}
+	std::cout << "Вы на " << floor << " этаже\n";
+	Elevator(floor - 1);
+	std::cout << "Вы на " << floor << " этаже\n";	
+}
+int Factorial(int n) 
+{
+	if (n == 0) return 1;
+	else return n * Factorial(n - 1);
+}
+
+double Power(double a, int n)
+{
+	if (a == 1 || a == 0) return a;
+	if (n > 1) return a * Power(a, n - 1);
+	if (n < 1) return 1 / a * Power(a, n + 1);
+	return a;
+}
+
+double Fibonacci(int n)
+{
+	if (n == 1 || n == 2) return (n - 1);
+	return Fibonacci(n - 1) + Fibonacci(n - 2);
+}
