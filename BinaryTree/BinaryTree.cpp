0879e2b472d86785f4c20b90399c4223f351e56b
Zello <Zello@DESKTOP-IBMII35>
2023-09-04 23:46:05 +0900

    BinaryTree

--- BinaryTree/BinaryTree.cpp
+++ BinaryTree/BinaryTree.cpp
@@ -12,82 +12,43 @@ protected:
 	public:
 		Element(int Data, Element* pLeft = nullptr, Element* pRaght = nullptr) : Data(Data), pLeft(pLeft), pRight(pRight)
 		{
+#ifdef DEBUG
 			std::cout << "EConstructor:\t" << this << std::endl;
+#endif // DEBUG
+
 		}
 		~Element()
 		{
-			std::cout << "EDestructor:\t" << this <<std::endl;
+#ifdef DEBUG
+			std::cout << "EDestructor:\t" << this << std::endl;
+#endif // DEBUG
+
 		}
 
 		friend class Tree;
 		friend class UniqueTree;
-		friend class TreeWithParameters;
-
 	}*Root;
 
-public:
-	Element* getRoot()
-	{
-		return Root;
-	}
-	Tree() :Root(nullptr)
-	{
-		std::cout << "EConstructor:\t" << this << std::endl;
-	}
-	Tree(const std::initializer_list<int>& list)
-	{
-		for (int i : list) 
-			insert(i);
-	}
-	~Tree()
-	{
-		clear_tree(Root);
-		std::cout << "TDestructor:\t" << this << std::endl;
-	}
-	void insert(int Data)
-	{
-		insert_param(Data, Root);
-	}
-	void insert_param(int Data, Element* Root)
-	{
-		if (this->Root == nullptr) this-> Root = new Element(Data);
-		if (Root == nullptr) return;
-		if (Data < Root->Data)
-		{
-			if (Root->pLeft == nullptr) Root->pLeft = new Element(Data);
-			else insert_param(Data, Root->pLeft);
-		}
-		else
-		{
-			if (Root->pRight == nullptr) Root->pRight = new Element(Data);
-			else insert_param(Data, Root->pRight);
-		}
-	}
-
-	void erase(int Data)
-	{
-		erase_param(Data, Root);
-	}
 	void erase_param(int Data, Element* Root)
 	{
 		Element* pPrev = nullptr;
 		Element* root = Root;
 
 		while (root != nullptr && root->Data != Data)
-		{			
+		{
 			pPrev = root;
 			if (Data < root->Data) root = root->pLeft;
 			else root = root->pRight;
 		}
 
 		if (root == nullptr) return;
-		
+
 		if (root->pLeft == nullptr && root->pRight == nullptr)
 		{
 			if (root != Root)
 			{
 				if (pPrev->pLeft == root) pPrev->pLeft = nullptr;
-				else pPrev->pRight = nullptr;			
+				else pPrev->pRight = nullptr;
 			}
 			else Root = nullptr;
 			delete root;
@@ -100,7 +61,7 @@ public:
 			erase_param(tmp_data, root);
 			root->Data = tmp_data;
 		}
-		else 
+		else
 		{
 			Element* pNext = (root->pLeft) ? root->pLeft : root->pRight;
 			if (root != Root)
@@ -112,20 +73,6 @@ public:
 			delete root;
 		}
 	}
-	void clear_tree(Element* Root)
-	{
-		if (Root != nullptr) 
-		{
-			clear_tree(Root->pLeft);
-			clear_tree(Root->pRight);
-			delete Root;
-		}
-	}
-
-	void print()
-	{		
-		print_param(Root);
-	}
 	void print_param(Element* Root)
 	{
 		if (Root == nullptr)return;
@@ -133,90 +80,159 @@ public:
 		std::cout << Root->Data << "\t";
 		print_param(Root->pRight);
 	}
-
-	void tree_print()
-	{
-		tree_print_param(Root, depth());
-	}
 	void tree_print_param(Element* Root, int depth)
 	{
 		if (Root == nullptr) return;
 		tree_print_param(Root->pLeft, depth + 1);
-		for (int i = 0; i < depth; i++) 
-			std::cout << "  ";
+		for (int i = 0; i < depth; i++)
+			std::cout << "   ";
 		std::cout << Root->Data << std::endl << std::endl;
 		tree_print_param(Root->pRight, depth + 1);
 	}
-
-	int minValue()
-	{
-		return minValue_param(Root);
-	}
-	int minValue_param(Element* Root)
-	{
-		return Root == nullptr ? 0 : Root->pLeft == nullptr ? Root->Data : minValue_param(Root->pLeft);
-	}
-
-	int maxValue()
-	{
-		return maxValue_param(Root);
-	}
-	int maxValue_param(Element* Root)
-	{
-		return Root == nullptr ? 0 : Root->pRight == nullptr ? Root->Data : maxValue_param(Root->pRight);
-	}
-
-	int sum()
-	{
-		return sum_param(Root);
-	}
-	int sum_param(Element* Root)
-	{
-		return Root == nullptr ? 0 : sum_param(Root->pLeft) + sum_param(Root->pRight) + Root->Data;
-	}
-
-	int count()
-	{
-		return count_param(Root);
-	}
-	int count_param(Element* Root)
-	{
-		return Root == nullptr ? 0 : 1 + count_param(Root->pLeft) + count_param(Root->pRight);
-	}
-
-	double Avg()
-	{
-		return (double)sum() / count();
-	}
-
-	int depth()
-	{
-		return depth_param(Root);
-	}
-	int depth_param(Element* Root)
-	{
-		if (Root == nullptr) return 0;
-		return Root->pLeft != nullptr ?  1 + depth_param(Root->pLeft) :  1 + depth_param(Root->pRight);
-	}
-};
-
-class UniqueTree : public Tree
-{
-public:
-	void insert(int Data, Element* Root)
+	void insert_param(int Data, Element* Root)
 	{
 		if (this->Root == nullptr) this->Root = new Element(Data);
 		if (Root == nullptr) return;
 		if (Data < Root->Data)
 		{
 			if (Root->pLeft == nullptr) Root->pLeft = new Element(Data);
-			else insert(Data, Root->pLeft);
+			else insert_param(Data, Root->pLeft);
+		}
+		else
+		{
+			if (Root->pRight == nullptr) Root->pRight = new Element(Data);
+			else insert_param(Data, Root->pRight);
+		}
+	}
+	int minValue_param(Element* Root)
+	{
+		return Root == nullptr ? 0 : Root->pLeft == nullptr ? Root->Data : minValue_param(Root->pLeft);
+	}
+	int maxValue_param(Element* Root)
+	{
+		return Root == nullptr ? 0 : Root->pRight == nullptr ? Root->Data : maxValue_param(Root->pRight);
+	}
+	int sum_param(Element* Root)
+	{
+		return Root == nullptr ? 0 : sum_param(Root->pLeft) + sum_param(Root->pRight) + Root->Data;
+	}
+	int count_param(Element* Root)
+	{
+		return Root == nullptr ? 0 : 1 + count_param(Root->pLeft) + count_param(Root->pRight);
+	}
+	int depth_param(Element* Root)
+	{
+		if (Root == nullptr) return 0;
+		return Root->pLeft != nullptr ? 1 + depth_param(Root->pLeft) : 1 + depth_param(Root->pRight);
+	}
+	void clear_tree_param(Element* Root)
+	{
+		if (Root == nullptr) return;
+		clear_tree_param(Root->pLeft);
+		clear_tree_param(Root->pRight);
+		delete Root;
+	}
+public:
+	Element* getRoot()
+	{
+		return Root;
+	}
+
+	//						Constructors
+
+	Tree() :Root(nullptr)
+	{
+#ifdef DEBUG
+		std::cout << "EConstructor:\t" << this << std::endl;
+#endif // DEBUG
+
+	}
+	Tree(const std::initializer_list<int>& list):Tree()
+	{
+		for (int i : list) 
+			insert(i);
+	}
+	~Tree()
+	{
+		clear_tree();
+#ifdef DEBUG
+		std::cout << "TDestructor:\t" << this << std::endl;
+#endif // DEBUG
+
+	}
+
+	//							Metods 
+
+
+	void insert(int Data)
+	{
+		insert_param(Data, Root);
+	}
+	void erase(int Data)
+	{
+		erase_param(Data, Root);
+	}	
+	void clear_tree()
+	{		
+		clear_tree_param(Root);
+		Root = nullptr;
+	}
+	void print()
+	{		
+		print_param(Root);
+	}
+	void tree_print()
+	{
+		tree_print_param(Root, depth());
+	}
+	int minValue()
+	{
+		return minValue_param(Root);
+	}
+	int maxValue()
+	{
+		return maxValue_param(Root);
+	}
+	int sum()
+	{
+		return sum_param(Root);
+	}
+	int count()
+	{
+		return count_param(Root);
+	}
+	double Avg()
+	{
+		return (double)sum() / count();
+	}
+	int depth()
+	{
+		return depth_param(Root);
+	}
+};
+
+class UniqueTree : public Tree
+{
+	void insert_param(int Data, Element* Root)
+	{
+		if (this->Root == nullptr) this->Root = new Element(Data);
+		if (Root == nullptr) return;
+		if (Data < Root->Data)
+		{
+			if (Root->pLeft == nullptr) Root->pLeft = new Element(Data);
+			else insert_param(Data, Root->pLeft);
 		}
 		else if (Data > Root->Data)
 		{
 			if (Root->pRight == nullptr) Root->pRight = new Element(Data);
-			else insert(Data, Root->pRight);
+			else insert_param(Data, Root->pRight);
 		}
+	}
+
+public:
+	void insert(int Data)
+	{
+		return insert_param(Data, Root);
 	}
 };
 
@@ -227,6 +243,7 @@ void main()
 {
 	setlocale(LC_ALL, "");
 	int n;
+	std::cout << "Введите размер дерева: "; std::cin >> n;
 
 #ifdef BASE_BINARYTREE
 
@@ -265,13 +282,16 @@ void main()
 
 	clock_t start = clock();	
 
-	Tree tree = { 8, 5, 3, 13, 21 };
+	Tree tree;// = { 50, 25, 75, 16, 32, 64, 90 };
+	for (int i = 0; i < n; i++)
+		tree.insert(rand() % 100);
 	tree.print();
+	//tree.insert(28);
 	std::cout << std::endl;
 	std::cout << "Глубина дерева: " << tree.depth();
 	std::cout << std::endl;
-	tree.erase(13);
-	tree.tree_print();
+	//tree.erase(13);
+	//tree.tree_print();
 	std::cout << std::endl;
 
 	clock_t end = clock();
