d654cee32d432dce4520496f551561c8c33c5a5a
Zello <Zello@DESKTOP-IBMII35>
2023-09-08 20:31:24 +0900

    balance and tree_print DONE

--- BinaryTree/BinaryTree.cpp
+++ BinaryTree/BinaryTree.cpp
@@ -297,7 +297,7 @@ void main()
 	std::cout << delim;
 	*/
 
-	Tree tree = { 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15 };
+	Tree tree = { 15,14,13,12,11,10,9,8,7,6,5,4,3,2,1 };
 	tree.tree_print();
 	std::cout << delim;
 	tree.balance();
